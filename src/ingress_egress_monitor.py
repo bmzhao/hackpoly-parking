4b142cc9395092a837d4b962de277bcfe9ca3b1b
Brian Zhao <bzspartan@gmail.com>
2017-02-12 09:13:36 -0800

    attempted merging of blobs

--- src/ingress_egress_monitor.py
+++ src/ingress_egress_monitor.py
@@ -6,7 +6,7 @@ import sys
 import state
 
 video_stream = 'http://169.254.225.251:8080/stream/video.mjpeg'
-minimum_area = 1000
+minimum_area = 8000
 
 
 def auto_canny(image, sigma=0.33):
@@ -55,6 +55,32 @@ if __name__ == '__main__':
     middle_line_right = (base_frame.shape[1], base_frame.shape[0] / 2)
     center_y_of_camera = base_frame.shape[0] / 2
 
+    # # Setup SimpleBlobDetector parameters.
+    # params = cv2.SimpleBlobDetector_Params()
+    #
+    # # Change thresholds
+    # params.minThreshold = 0
+    # params.maxThreshold = 9999
+    #
+    # # Filter by Area.
+    # params.filterByArea = True
+    # params.minArea = 1500
+    #
+    # # Filter by Circularity
+    # params.filterByCircularity = True
+    # params.minCircularity = 0.1
+    #
+    # # Filter by Convexity
+    # params.filterByConvexity = True
+    # params.minConvexity = 0.87
+    #
+    # # Filter by Inertia
+    # params.filterByInertia = True
+    # params.minInertiaRatio = 0.01
+    #
+    # # Create a detector with the parameters
+    # detector = cv2.SimpleBlobDetector(params)
+
 
     start_time = time.time()
     previous_blobs = []
@@ -69,7 +95,7 @@ if __name__ == '__main__':
         # frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
         original_frame = frame.copy()
-        cv2.rectangle(original_frame, middle_line_left, middle_line_right, (255, 255, 255), thickness=5)
+        cv2.rectangle(original_frame, middle_line_left, middle_line_right, (0, 0, 255), thickness=5)
 
         frame = cv2.GaussianBlur(frame, (5, 5), 2)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -82,54 +108,77 @@ if __name__ == '__main__':
         frame = cv2.dilate(frame, kernel, iterations=3)
         frame = cv2.dilate(frame, small_kernel, iterations=1)
         # frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
+        # keypoints = detector.detect(frame)
 
 
         contours, hierarchy = cv2.findContours(frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        # original_frame = cv2.drawKeypoints(frame, keypoints, np.array([]), 255,
+        #                           cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         potential_cars = filter(lambda contour: cv2.contourArea(contour) > minimum_area, contours)
 
         new_blobs = []
         for new_blob in potential_cars:
             x, y, w, h = cv2.boundingRect(new_blob)
-            center_y = y + h / 2
-            center_x = x + w / 2
-            # print 'ratio', float(w)/h
-            # rect = cv2.minAreaRect(biggestContour)
-            # print rect
-            # print 'actual ratio: ', float(rect[1][0])/rect[1][1]
-
-            cv2.rectangle(original_frame, (x, y), (x + w, y + h), 255, 2)
-            # print 'area: ', cv2.contourArea(new_blob)
-            new_blobs.append(state.Blob((center_x,center_y),(x,y,w,h)))
-
-
-            # if center_y > center_y_of_camera and num_blobs_past_line == 0:
-            #     num_blobs_past_line += 1
-            #     print 'car passed line'
-            #     print requests.post('http://10.110.165.48:9000/lots', json={'lot': 'J', 'diff': 1})
+            center_y = y + (h / 2)
+            center_x = x + (w / 2)
+            # cv2.rectangle(original_frame, (x, y), (x + w, y+h), 255, 2)
+            new_blobs.append(state.Blob(center_x, center_y, w, h, cv2.contourArea(new_blob)))
 
         # merge blobs
+        to_delete = set()
+        new_blobs.sort(key=lambda blob: blob.area, reverse=True)
+        for new_blob in new_blobs:
+            if new_blob not in to_delete:
+                for new_blob_1 in new_blobs:
+                    if new_blob_1 not in to_delete and new_blob_1 is not new_blob and \
+                            new_blob.close_to(new_blob_1):
+                        new_blob.merge(new_blob_1)
+                        # print 'merge occurred'
+                        to_delete.add(new_blob_1)
 
-        #todo fix this, wrong assumption
-        # if len(previous_blobs) == 0:
-        #     previous_blobs = new_blobs
-        # else:
-            # for new_blob in new_blobs:
-            #     closest_previous_blob = previous_blobs[0]
-            #     dist = new_blob.dist(closest_previous_blob)
-            #     for previous_blob in previous_blobs[1:]:
-            #         new_dist = previous_blob.dist(new_blob)
-            #         if new_dist < dist:
-            #             dist = new_dist
-            #             closest_previous_blob = previous_blob
-            #     previous_blobs.remove(closest_previous_blob)
-            #     if closest_previous_blob.centroid[1] < center_y_of_camera and new_blob.centroid[1] > center_y_of_camera:
-            #         print 'car entered parking lot!'
-            #         print requests.post('http://10.110.165.48:9000/lots', json={'lot': 'J', 'diff': 1})
-            #     elif closest_previous_blob.centroid[1] > center_y_of_camera and new_blob.centroid[1] < center_y_of_camera:
-            #         print 'car left parking lot!'
-            #         print requests.post('http://10.110.165.48:9000/lots', json={'lot': 'J', 'diff': -1})
+        for new_blob in to_delete:
+            # print 'deleted stuff'
+            new_blobs.remove(new_blob)
 
+        for new_blob in new_blobs:
+            cv2.rectangle(original_frame, (int(new_blob.x - new_blob.w/2), int(new_blob.y - new_blob.h/2)),
+                          (int(new_blob.x + new_blob.w/2), int(new_blob.y + new_blob.h/2)), 255, 2)
+
+        # numBeforeTop = len(filter(lambda blob: blob.centroid[1] < center_y_of_camera,previous_blobs))
+        # numBeforeBottom = len(previous_blobs) - numBeforeTop
+        #
+        # numAfterTop = len(filter(lambda blob: blob.centroid[1] < center_y_of_camera,new_blobs))
+        # numAfterBottom = len(new_blobs) - numAfterTop
+        #
+        # numLeft = numAfterTop - numBeforeTop
+        # numEntered = numAfterBottom - numBeforeBottom
+        # total = numEntered - numLeft
+        # if total != 0:
+        #     print total, 'cars entered'
+        #     print requests.post('http://10.110.165.48:9000/lots', json={'lot': 'J', 'diff': total})
+        # previous_blobs = new_blobs
+
+        # todo fix this, wrong assumption
+
+        for new_blob in new_blobs:
+            if len(previous_blobs) != 0:
+                closest_previous_blob = previous_blobs[0]
+                dist = new_blob.dist(closest_previous_blob)
+                for previous_blob in previous_blobs[1:]:
+                    new_dist = previous_blob.dist(new_blob)
+                    if new_dist < dist:
+                        dist = new_dist
+                        closest_previous_blob = previous_blob
+                previous_blobs.remove(closest_previous_blob)
+                if closest_previous_blob.y < center_y_of_camera and new_blob.y > center_y_of_camera:
+                    print 'car entered parking lot!'
+                    print requests.post('http://10.110.165.48:9000/lots', json={'lot': 'J', 'diff': 1})
+                elif closest_previous_blob.y > center_y_of_camera and new_blob.y < center_y_of_camera:
+                    print 'car left parking lot!'
+                    print requests.post('http://10.110.165.48:9000/lots', json={'lot': 'J', 'diff': -1})
+
+        previous_blobs = new_blobs
 
         cv2.imshow('original', original_frame)
         cv2.imshow('frame', frame)
--- src/state.py
+++ src/state.py
@@ -7,17 +7,14 @@ class VisualState(object):
         self.blobs = blobs
 
 
-
-
-
-
-
 class Blob(object):
-    def __init__(self, (center_x,center_y),(x,y,w,h)):
+    def __init__(self, x, y, w, h, area):
         self.id = str(uuid.uuid4())
-        self.centroid = (center_x, center_y)
-        self.boundingRect = (x,y,w,h)
-
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.area = area
 
     def __hash__(self):
         return hash(self.id)
@@ -29,4 +26,54 @@ class Blob(object):
         return not (self == other)
 
     def dist(self, other):
-        return math.hypot(self.centroid[0] - other.centroid[0],self.centroid[1]- other.centroid[1] )
+        return math.hypot(self.x - other.x, self.y - other.y)
+
+    def close_to(self, other, ratio=0.25):
+        right = self.x + ratio * self.w/2
+        left = self.x - ratio * self.w/2
+
+        bottom = self.y - ratio * self.h/2
+        top = self.y + ratio * self.h/2
+
+        otherBottom = other.y - other.h/2
+        otherTop = other.y + other.h/2
+
+        otherRight = other.x + other.w/2
+        otherLeft = other.x - other.w/2
+
+        if (otherBottom < top and otherBottom > bottom) or \
+                (otherTop < top and otherTop > bottom) or \
+                (otherRight < right and otherRight > left) or \
+                (otherLeft < right and otherRight > left):
+            return True
+        else:
+            return False
+
+    def merge(self, other):
+        right = self.x + self.w / 2
+        left = self.x - self.w / 2
+
+        bottom = self.y - self.h / 2
+        top = self.y + self.h / 2
+
+        otherBottom = other.y - other.h / 2
+        otherTop = other.y + other.h / 2
+
+        otherRight = other.x + other.w / 2
+        otherLeft = other.x - other.w / 2
+
+        newRight = max(right, otherRight)
+        newLeft = min(left, otherLeft)
+        newTop = max(top, otherTop)
+        newBottom = min(bottom, otherBottom)
+
+        newCenterX = (newRight + newLeft) / 2
+        newCenterY = (newTop + newBottom) / 2
+
+        newWidth = (newRight - newLeft) / 2 / 1.2
+        newHeight = (newTop - newBottom) / 2 / 1.2
+        self.x = newCenterX
+        self.y = newCenterY
+        self.area += other.area
+        self.w = newWidth
+        self.h = newHeight
